c233b6b261fb96888311942bca24ecdbf00b1a85
LePhuThanh <lpthanh710@gmail.com>
2026-07-16 22:10:42 +0700

    first init

---
+++ src/main/java/com/phelim/gateway_vertx_project/GatewayVertxProjectApplication.java
@@ -0,0 +1,13 @@
+package com.phelim.gateway_vertx_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayVertxProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayVertxProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/phelim/gateway_vertx_project/GatewayVertxProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phelim.gateway_vertx_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayVertxProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
